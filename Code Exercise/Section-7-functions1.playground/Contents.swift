c974fa3821314dbb2fe61a52b9f1330348e810af
sbpark88 <saebyulpark88@gmail.com>
2022-06-16 08:03:56 +0900

    Add: Code exercise! How packing functions
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Using and Understanding Apple Documentation, Section 7

---
+++ Code Exercise/Section-7-functions1.playground/Contents.swift
@@ -0,0 +1,106 @@
+import UIKit
+
+//Don't change any of this code.
+var innerArray = ["⬜️", "⬜️", "⬜️", "⬜️", "⬜️", "⬜️", "⬜️"]
+
+var outerArray = [[String]]()
+
+var outerIndex = 0
+var innerIndex = 0
+
+
+func createWorld() {
+    for _ in 0...6 {
+        outerArray.append([])
+    }
+    for i in 0...6 {
+        outerArray[i] = innerArray
+    }
+}
+
+
+func start() {
+    createWorld();
+    outerArray[0][0] = "🦊"
+    outerArray[5][4] = "🌽"
+    for i in 0...6 {
+        print(outerArray[i])
+    }
+}
+
+func locateFox() {
+    for i in 0..<outerArray.count - 1 {
+        if let n1 = outerArray.firstIndex(where: {$0[i] ==  "🦊"})  {
+            outerIndex = n1
+        }
+        if let n2 = outerArray[outerIndex].firstIndex(where: {$0 ==  "🦊"}) {
+            innerIndex = n2
+        }
+        //        print("\(outerIndex)\(innerIndex)")
+    }
+}
+
+func left() {
+    locateFox()
+    if innerIndex > 0 {
+       outerArray[outerIndex][innerIndex] = "⬜️"
+       outerArray[outerIndex][innerIndex - 1] = "🦊"
+    } else {
+      print("can't go further left.")
+    }
+}
+
+func right() {
+    locateFox()
+    if (innerIndex < innerArray.count) {
+      outerArray[outerIndex][innerIndex] = "⬜️"
+      outerArray[outerIndex][innerIndex + 1] = "🦊"
+    } else {
+      print("Can't go further right.")
+    }
+}
+
+func up() {
+    locateFox()
+    if outerIndex > 0 {
+      outerArray[outerIndex][innerIndex] = "⬜️"
+      outerArray[outerIndex - 1][innerIndex] = "🦊"
+    } else {
+      print("Can't go further up.")
+    }
+}
+
+
+func down() {
+    locateFox()
+    if outerIndex < outerArray.count {
+      outerArray[outerIndex][innerIndex] = "⬜️"
+      outerArray[outerIndex + 1][innerIndex] = "🦊"
+    } else {
+      print("Can't go further down.")
+    }
+}
+
+
+func visualise() {
+    for i in 0...6 {
+        print(outerArray[i])
+    }
+}
+
+
+
+
+
+print("Starting map")
+start()
+
+//Write your code here:
+
+
+
+
+//Don't change the code below this line.
+
+print("Final map")
+visualise();
