171b69d8b95d26355708ded8d8f03f6343383cfb
sbpark88 <saebyulpark88@gmail.com>
2022-06-17 12:06:53 +0900

    Update: My answer of the Calculator
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Using and Understanding Apple Documentation, Section 7

--- Code Exercise/Section-7-functions2/Section-7-functions2/main.swift
+++ Code Exercise/Section-7-functions2/Section-7-functions2/main.swift
@@ -10,16 +10,23 @@ import Foundation
 
 //Don't change this code:
 func calculator() {
-  let a = Int(readLine()!)! //First input
-  let b = Int(readLine()!)! //Second input
-
-  add(n1: a, n2: b)
-  subtract(n1: a, n2: b)
-  multiply(n1: a, n2: b)
-  divide(n1: a, n2: b)
-
+    print("첫 번째 정수를 입력해주세요.")
+    let a = Int(readLine()!)! //First input
+    print("두 번째 정수를 입력해주세요.")
+    let b = Int(readLine()!)! //Second input
+    print("")
+    
+    add(n1: a, n2: b)
+    subtract(n1: a, n2: b)
+    multiply(n1: a, n2: b)
+    divide(n1: a, n2: b)
+    
 }
 
+func add(n1: Int, n2: Int) -> Void { print("n1 + n2 = \(n1 + n2)") }
+func subtract(n1: Int, n2: Int) -> Void { print("n1 - n2 = \(n1 - n2)") }
+func multiply(n1: Int, n2: Int) -> Void { print("n1 * n2 = \(n1 * n2)") }
+func divide(n1: Int, n2: Int) -> Void { print("n1 / n2 = \(Double(n1) / Double(n2))") }
 
 //Write your code below this line to make the above function calls work.
 calculator()
