6cbc2859e59529ecba3b97dc5d60a0ef2c98e2b1
sbpark88 <saebyulpark88@gmail.com>
2022-06-22 00:19:32 +0900

    Refactor: Code exercise if else
    Generate random int number using range operator.
    
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Control Flow and Optionals, Section 8

--- Code Exercise/Section-8-If-Else.playground/Contents.swift
+++ Code Exercise/Section-8-If-Else.playground/Contents.swift
@@ -1,14 +1,15 @@
 import UIKit
 import Darwin
 
-let loveScore:Int = loveCalculator()
-
-if loveScore == 100 {
-    print("You love each other like Kanye loves Kanye.")
-} else {
-    print("You'll be forever alone.")
+func loveCalculator() {
+    let loveScore:Int = Int.random(in: 1...100)
+    
+    
+    if loveScore == 100 {
+        print("You love each other like Kanye loves Kanye.")
+    } else {
+        print("You'll be forever alone.")
+    }
 }
 
-func loveCalculator() -> Int {
-    return Int(arc4random_uniform(101))
-}
+loveCalculator()
