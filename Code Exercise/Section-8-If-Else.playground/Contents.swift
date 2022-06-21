6d7fb3a7487bd818f53e92897e41b05f0b58811d
sbpark88 <saebyulpark88@gmail.com>
2022-06-22 00:23:26 +0900

    Update: Code exercise if else if and else
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Control Flow and Optionals, Section 8

--- Code Exercise/Section-8-If-Else.playground/Contents.swift
+++ Code Exercise/Section-8-If-Else.playground/Contents.swift
@@ -5,8 +5,10 @@ func loveCalculator() {
     let loveScore:Int = Int.random(in: 1...100)
     
     
-    if loveScore == 100 {
+    if loveScore > 80 {
         print("You love each other like Kanye loves Kanye.")
+    } else if (loveScore <= 80) && (loveScore >= 40) {
+        print("You go together like Coke and Mentos.")
     } else {
         print("You'll be forever alone.")
     }
