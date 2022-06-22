9ba182e7cb1f21b3bb95c2dcef1dd9ca8d0f247e
sbpark88 <saebyulpark88@gmail.com>
2022-06-22 23:35:54 +0900

    Update: Code exercise! Change if-else to switch-case
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Control Flow and Optionals, Section 8

--- Code Exercise/Section-8-If-Else.playground/Contents.swift
+++ Code Exercise/Section-8-If-Else.playground/Contents.swift
@@ -2,16 +2,37 @@ import UIKit
 import Darwin
 
 func loveCalculator() {
-    let loveScore:Int = Int.random(in: 1...100)
-    
-    
-    if loveScore > 80 {
+    loveCalculator(loveScore: Int.random(in: 1...100))
+}
+
+func loveCalculator(loveScore: Int) {
+    /*if loveScore > 80 {
         print("You love each other like Kanye loves Kanye.")
     } else if loveScore >= 40 {
         print("You go together like Coke and Mentos.")
     } else {
         print("You'll be forever alone.")
+    }*/
+
+    switch loveScore {
+        case 1..<40:
+            print("You'll be forever alone.")
+        case 40..<80:
+            print("You go together like Coke and Mentos.")
+        default:
+        print("You love each other like Kanye loves Kanye.")
     }
+    
 }
 
+loveCalculator(loveScore: 39)   // You'll be forever alone.
+loveCalculator(loveScore: 40)   // You go together like Coke and Mentos.
+loveCalculator(loveScore: 41)   // You go together like Coke and Mentos.
+loveCalculator(loveScore: 79)   // You go together like Coke and Mentos.
+loveCalculator(loveScore: 80)   // You go together like Coke and Mentos.
+loveCalculator(loveScore: 81)   // You love each other like Kanye loves Kanye.
+
 loveCalculator()
+
+
+
