c5c30bbd183906f5a0ea03bf0d08c76fdd059f19
sbpark88 <saebyulpark88@gmail.com>
2022-06-23 07:20:38 +0900

    Update: Print egg time on button pushed
    Solve this challenge using switch case.
    
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Control Flow and Optionals, Section 8

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+        
+    @IBAction func pressButton(_ sender: UIButton) {
+        
+        var hardness: Any
+
+        switch sender.currentTitle! {
+        case "Soft":
+            hardness = softTime
+            break
+        case "Medium":
+            hardness = mediumTime
+            break
+        case "Hard":
+            hardness = hardTime
+            break
+        default:
+            hardness = "Error"
+        }
+
+        print(hardness)
+
+    }
 
 }
