1b0ef8fe9ebe9174de54800c36e3a76075c6c5f5
sbpark88 <saebyulpark88@gmail.com>
2022-06-25 00:36:53 +0900

    Update: Make timer to act when button is triggered
    # Two ways of the optional binding to unwarp the optional.
    - guard let (boolean condition) { control transfer statement
      such as return, break, continue, or throw }
    - if let (boolean condition) { when true } else { when false }
    
    # Extracting closures from the block parameter in the method.
    
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Control Flow and Optionals, Section

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,15 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer: Timer = Timer()
     let eggTime: [String: Int] = [
         "Soft": 5,
         "Medium": 7,
         "Hard": 12
     ]
-    
-    @IBAction func pressButton(_ sender: UIButton) {
-        
-        print(eggTime[sender.currentTitle!] ?? "Error")
 
+    @IBAction func pressButton(_ sender: UIButton) {
+
+        let eggTimeTitle = sender.currentTitle!
+        //        print(eggTime[eggTimeTitle] ?? "Error")
+
+        if eggTimeTitle == "Hard" {
+            // Solution 1
+            // Optional biniding(unwrap) using guard
+            // This looks like more EAFP(“It’s Easier to Ask Forgiveness than Permission”) for me.
+            guard let time = eggTime[eggTimeTitle] else {
+                return
+            }
+            startTimer(time: time)
+        } else {
+            // Solution 2
+            // Optional biniding(unwrap) using if
+            // This looks like more LBYL(Look Before You Leap) for me.
+            if let time = eggTime[eggTimeTitle] {
+                startTimer(time: time)
+            }
+        }
+    }
+
+    private func startTimer(time: Int) {
+        var remainTime = time
+
+        // Make previous trigger expired.
+        timer.invalidate()
+        printTimer(time: remainTime)
+        if time > 10 {
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { Timer in
+                if remainTime > 1 {
+                    remainTime -= 1
+                    self.printTimer(time: remainTime)
+                } else {
+                    print("0! Enjoy yourself~~")
+                    Timer.invalidate()
+                }
+            })
+        } else {
+            // This code same with in if clause. We can extract closures instead of block parameters in 'Time.scheduledTimer'
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { Timer in
+                if remainTime > 1 {
+                    remainTime -= 1
+                    self.printTimer(time: remainTime)
+                } else {
+                    print("0! Enjoy yourself~~")
+                    Timer.invalidate()
+                }
+            }
+        }
+    }
+
+    private func printTimer(time: Int) {
+//        print(time, " seconds")
+        print("\(time) seconds")
     }
 }
