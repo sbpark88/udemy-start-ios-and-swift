aa7465403ea57112e24b56024db6a36cb808dce9
sbpark88 <saebyulpark88@gmail.com>
2022-06-27 07:54:17 +0900

    Update: Make IBOutlet and IBAction with function
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : iOS App Design Patterns and Code Structuring, Section 9

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Four + Two is equal to Six.",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        printQuiz(number: questionNumber)
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        printQuiz(number: questionNumber + 1)
+    }
+    
+    private func printQuiz(number: Int) {
+        questionLabel.text = quiz[number]
+    }
+    
 }
 
