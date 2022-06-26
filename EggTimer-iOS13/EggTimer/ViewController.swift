f2e133c5ca3596c0cb2f5898b1ec495e6db375c7
sbpark88 <saebyulpark88@gmail.com>
2022-06-26 21:41:20 +0900

    Update: My Answer of the "Egg Timer" progress bar
    # printTimer function is changed to nested function
    - therefore, closure can access without the "self".
    
    # change if else clause
    - It has become more concise.
    
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Control Flow and Optionals, Section

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -17,10 +17,15 @@ class ViewController: UIViewController {
         "Hard": 12
     ]
 
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var progressText: UILabel!
+
     @IBAction func pressButton(_ sender: UIButton) {
 
         let eggTimeTitle = sender.currentTitle!
         //        print(eggTime[eggTimeTitle] ?? "Error")
+
 
         if eggTimeTitle == "Hard" {
             // Solution 1
@@ -41,37 +46,48 @@ class ViewController: UIViewController {
     }
 
     private func startTimer(time: Int) {
-        var remainTime = time
+        let totalTime: Float = Float(time) // * 60
+        var remainTime = time // * 60
 
-        // Make previous trigger expired.
+        // Init. Make previous trigger expired.
         timer.invalidate()
-        printTimer(time: remainTime)
+        progressBar.progress = 0
+
+        // Execute only one they are started.
+        setTitleText(text: "The eggs are being bioled.")
+        progressText.text = "\(remainTime) secodns"
+
         if time > 10 {
+            // Solution 1
             timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { Timer in
-                if remainTime > 1 {
-                    remainTime -= 1
-                    self.printTimer(time: remainTime)
-                } else {
-                    print("0! Enjoy yourself~~")
+                remainTime -= 1
+                proceedProgressBar(time: remainTime)
+                if remainTime == 0 {
+                    self.setTitleText(text: "Enjoy yourself!")
                     Timer.invalidate()
                 }
             })
         } else {
+            // Solution 2
             // This code same with in if clause. We can extract closures instead of block parameters in 'Time.scheduledTimer'
-            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { Timer in
-                if remainTime > 1 {
-                    remainTime -= 1
-                    self.printTimer(time: remainTime)
-                } else {
-                    print("0! Enjoy yourself~~")
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] Timer in
+                remainTime -= 1
+                proceedProgressBar(time: remainTime)
+                if remainTime == 0 {
+                    setTitleText(text: "Enjoy yourself!")
                     Timer.invalidate()
                 }
             }
         }
+
+        func proceedProgressBar(time: Int) {
+            progressText.text = time != 0 ? "\(time) secodns" : "Done!"
+            progressBar.progress += 1 / totalTime
+        }
     }
 
-    private func printTimer(time: Int) {
-//        print(time, " seconds")
-        print("\(time) seconds")
+    func setTitleText(text: String) {
+        titleLable.text = text
     }
+
 }
