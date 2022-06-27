2528b9c4906a36f7e276d172d2d6bab23d346719
sbpark88 <saebyulpark88@gmail.com>
2022-06-28 06:39:13 +0900

    Update: Print answer of the quiz and make it infinite
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
    : iOS App Design Patterns and Code Structuring, Section 9

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -9,33 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
-    
-    let quiz = [
-        "Four + Two is equal to Six.",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"
-    ]
-    
+
+    let _true = "True"
+    let _false = "False"
+
+    var quiz: [[String]] = []
+
     var questionNumber = 0
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        quiz = [
+            ["Four + Two is equal to Six.", _true],
+            ["Five - Three is greater than One", _true],
+            ["Three + Eight is less than Ten", _false]
+        ]
+
         printQuiz(number: questionNumber)
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        printQuiz(number: questionNumber + 1)
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+
+        if userAnswer == actualAnswer {
+            print("Right")
+        } else {
+            print("Wrong!")
+        }
+
+        userAnswer == actualAnswer ? print("Right") : print("Wrong!")
+
+        questionNumber = questionNumber + 1 < quiz.count
+                ? questionNumber + 1
+                : 0
+
+        printQuiz(number: questionNumber)
     }
-    
+
     private func printQuiz(number: Int) {
-        questionLabel.text = quiz[number]
+        questionLabel.text = quiz[number][0]
     }
-    
+
 }
 
