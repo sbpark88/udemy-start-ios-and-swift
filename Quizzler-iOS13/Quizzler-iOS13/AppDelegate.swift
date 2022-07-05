5b106da90ffd9c7c7a77fa56d5228c9e6929713f
sbpark88 <saebyulpark88@gmail.com>
2022-07-05 22:50:25 +0900

    Chore: Make fulfill the SwiftLint's rules
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
    : iOS App Design Patterns and Code Structuring, Section 9

--- Quizzler-iOS13/Quizzler-iOS13/AppDelegate.swift
+++ Quizzler-iOS13/Quizzler-iOS13/AppDelegate.swift
@@ -8,10 +8,9 @@
 
 import UIKit
 
+// swiftlint:disable line_length
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -36,6 +35,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
