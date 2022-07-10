f1d74588af36a609649ef28d2c4c92ea0e0f9269
sbpark88 <saebyulpark88@gmail.com>
2022-07-10 23:18:47 +0900

    Update: Link the slider to the UI updater functions
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Advanced Swift Programming - Classes, Inheritance
        & Advanced Optionals, Section 11

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var heightLable: UILabel!
+    @IBOutlet weak var weightLable: UILabel!
+
+    var bodyInfo: [UILabel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bodyInfo = [heightLable, weightLable]
     }
 
+    @IBAction func bodyInformationChanged(_ sender: UISlider) {
+        updateUi(tag: sender.tag, measurement: sender.value)
+    }
+
+    private func updateUi(tag: Int, measurement: Float) {
+//        let strMeasurement: String = String(round(measurement * 10) / 10)
+        let strMeasurement: String = String(format: "%.1f", measurement)
+
+        bodyInfo[tag].text = tag == 0
+                ? "\(strMeasurement) cm"
+                : "\(strMeasurement) kg"
+    }
 
 }
-
