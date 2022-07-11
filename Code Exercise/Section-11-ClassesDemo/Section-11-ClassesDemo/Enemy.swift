3d7c6e5b731946be26766b10a4953b9b5565047d
sbpark88 <saebyulpark88@gmail.com>
2022-07-12 00:31:25 +0900

    Add: Understanding about swift classes and inheritance
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Advanced Swift Programming - Classes, Inheritance
        & Advanced Optionals, Section 11

---
+++ Code Exercise/Section-11-ClassesDemo/Section-11-ClassesDemo/Enemy.swift
@@ -0,0 +1,18 @@
+class Enemy {
+    var health: Int
+    var attackStregth: Int
+
+    init(health: Int, attackStrength: Int) {
+        self.health = health
+        self.attackStregth = attackStrength
+    }
+
+    func move() {
+        print("Walk forwards.")
+    }
+
+    func attack() {
+        print("Land a hit, does \(attackStregth) damage.")
+    }
+
+}
