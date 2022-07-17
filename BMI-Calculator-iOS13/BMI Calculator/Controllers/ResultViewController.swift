2ef7067a0152ab520b0626fc8355c02a356d3ce5
sbpark88 <saebyulpark88@gmail.com>
2022-07-18 08:13:03 +0900

    Close: Finish my BMI app
    I have known access UI elements without IBOutlet now.
    - Use 'view.viewWithTag()'!!
    
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
      : Advanced Swift Programming - Classes, Inheritance
        & Advanced Optionals, Section 11

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultViewController.swift
@@ -14,6 +14,8 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var adviceLabel: UILabel!
 
     var bmiValue: String?
+    var advice: String?
+    var backgroundColor: UIColor?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,21 @@ class ResultViewController: UIViewController {
             fatalError("guard failure handling has not been implemented")
         }
         bmiLabel.text = bmiValue
+
+        guard let advice = advice else {
+            fatalError("guard failure handling has not been implemented")
+        }
+        adviceLabel.text = advice
+
+        /*guard let background = view.viewWithTag(1) as? UIImageView else {
+            fatalError("guard failure handling has not been implemented")
+        }
+        background.backgroundColor = backgroundColor*/
+
+        guard let backgroundColor = backgroundColor else {
+            fatalError("guard failure handling has not been implemented")
+        }
+        view.backgroundColor = backgroundColor
     }
 
     @IBAction func recalculatePressed(_ sender: UIButton) {
---
+++ BMI-Calculator-iOS13/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,12 @@
+//
+// Created by 박새별 on 2022/07/17.
+// Copyright (c) 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
