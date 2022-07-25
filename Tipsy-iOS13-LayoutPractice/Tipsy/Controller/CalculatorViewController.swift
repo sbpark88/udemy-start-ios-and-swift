d0dbea2964e1f454fab26ebebdcd42e7bc925f48
sbpark88 <saebyulpark88@gmail.com>
2022-07-25 22:51:36 +0900

    Update: Create IBActions and IBOutlets
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
     : Advanced Swift Programming Challenge, Section 12

---
+++ Tipsy-iOS13-LayoutPractice/Tipsy/Controller/CalculatorViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
