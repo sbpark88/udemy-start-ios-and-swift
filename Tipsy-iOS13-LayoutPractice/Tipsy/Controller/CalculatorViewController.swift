328c31a10554e3613f82202d9c8c1bf649e42cfb
sbpark88 <saebyulpark88@gmail.com>
2022-08-01 23:18:45 +0900

    Update: Calculate the Bill
    Check the text is in number format using Regex.
    
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
     : Advanced Swift Programming Challenge, Section 12

--- Tipsy-iOS13-LayoutPractice/Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy-iOS13-LayoutPractice/Tipsy/Controller/CalculatorViewController.swift
@@ -5,6 +5,7 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet var tipPercentButton: [UIButton]!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    let numberRegex: String = "^(0?\\.\\d+|[1-9]\\d*\\.?\\d*)$"
 
     @IBAction func tipChanged(_ sender: UIButton) {
         tipPercentButton.forEach { button in
@@ -17,6 +18,25 @@ class CalculatorViewController: UIViewController {
     }
 
     @IBAction func calculatePressed(_ sender: UIButton) {
+        guard let billTotal = billTextField.text else {
+            fatalError("guard failure handling has not been implemented")
+        }
+
+        billTextField.endEditing(true)  // focus out
+
+        // 'billTotal' can pass the guard let condition when ""(empty string)
+        // Therefore, we must check it.
+        // In this code, I used regex to check is it number format.
+        if billTotal.range(of: numberRegex, options: .regularExpression) != nil {
+            var eachTip: Double = 0.0
+            tipPercentButton.forEach { button in
+                if button.isSelected {
+                    eachTip = Double(billTotal)! * (Double(button.tag) / 100) / Double(splitNumberLabel.text!)!
+                }
+            }
+
+            print(String(format: "%.2f", eachTip))
+        }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
