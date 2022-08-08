6bf97ca52352bc7a317957102480345a944f2038
sbpark88 <saebyulpark88@gmail.com>
2022-08-08 23:58:36 +0900

    Update: The usage of the UITextFieldDelegate
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
    : Networking, JSON Parsing, APIs and Core Location, Section 13

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
+    @IBAction func searchPressed(_ sender: Any) {
+        print(searchTextField.text!)
+    }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        fatalError("textFieldShouldReturn(_:) has not been implemented")
+    }
 }
 
