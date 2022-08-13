20a5a7469b95d3ab39c8765d24f25c0d5b418663
sbpark88 <saebyulpark88@gmail.com>
2022-08-13 23:56:59 +0900

    Update: Perform http request in my app
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
    : Networking, JSON Parsing, APIs and Core Location, Section 13

--- Code Exercise/WeatherManager.swift
+++ Code Exercise/WeatherManager.swift
@@ -8,4 +8,23 @@ struct WeatherManager {
         let urlString = "\(weatherUrl)&q=\(cityName)"
         print(urlString)
     }
+
+    func performRequest(urlString: String) {
+        // 1. Create a URL
+        guard let url = URL(string: urlString) else { return }
+
+        // 2. Create a URLSession
+        let session = URLSession(configuration: .default)
+
+        // 3. Give the session a task
+        let task: URLSessionDataTask = session.dataTask(with: urlString, completionHandler: <#T##@escaping (Data?, URLResponse?, Error?) -> Void##@escaping (Foundation.Data?, Foundation.URLResponse?, Swift.Error?) -> Swift.Void#>)
+
+        // 4. Start the task
+        task.resume()
+
+        func handle(data: Data?, response: URLResponse?, error: Error?) {
+
+        }
+
+    }
 }
