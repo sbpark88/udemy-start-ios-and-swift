0d02eaac800604be6b2984f5bd2e030f216de17c
sbpark88 <saebyulpark88@gmail.com>
2022-09-20 22:35:39 +0900

    Update: Set up currencyPicker in ViewController
    # Udemy iOS 13 & Swift
    - lecture: Angela Yu
    - Intermediate Swift Programming
    : Networking and API Challenge, Section 14

--- ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoinLabel: UIView!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+
+    var coinMnagager = CoinManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
 
+}
 
+// MARK: UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+
+    // How many pickers do you provide for?
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+
+    // What is the length of the pickers?
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        coinMnagager.currencyArray.count
+    }
+
+}
+
+// MARK: UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        coinMnagager.currencyArray[row]
+    }
 }
 
