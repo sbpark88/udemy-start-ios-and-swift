b342fb07e75db7deee0fb24388a8aa95316e3b96
sbpark88 <saebyulpark88@gmail.com>
2023-03-12 11:07:51 +0900

    Feat: Firebase Auth Sign-out & Customizing Navigation
    - Firebase Auth Sign-out
    - Append Navigation Title
    - Append Navigation Button
    - Hide Navigation Button

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
@@ -15,10 +16,20 @@ class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        title = "⚡️FlashChat"
     }
 
     @IBAction private func sendPressed(_ sender: UIButton) {
     }
 
+    @IBAction func signOutPress(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %0", signOutError)
+        }
+    }
 }
