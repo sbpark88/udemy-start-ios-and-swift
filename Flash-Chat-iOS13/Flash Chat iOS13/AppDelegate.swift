bb17e4af2c11f8180e97e294a041263ccf0bce30
sbpark88 <saebyulpark88@gmail.com>
2023-03-15 07:16:26 +0900

    Add: Add dependency - IQKeyboardManager
    # Add dependency using SPM
    
    # My Default Settings
    - IQKeyboardManager.shared.enable = true
    - IQKeyboardManager.shared.enableAutoToolbar = false
    - IQKeyboardManager.shared.shouldResignOnTouchOutside = true

--- Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
@@ -10,12 +10,16 @@ import UIKit
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         FirebaseApp.configure()
         let db = Firestore.firestore()
         print(db)
