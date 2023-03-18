a7f63c6a6fa2e9fea21c26b16747a07db9602850
sbpark88 <saebyulpark88@gmail.com>
2023-03-19 04:33:46 +0900

    Add: Dicee-SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by 박새별 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
