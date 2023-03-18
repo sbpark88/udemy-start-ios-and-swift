76ba3b40b5d49c94e4cd513cc7179623c88d420c
sbpark88 <saebyulpark88@gmail.com>
2023-03-19 06:59:01 +0900

    Close: Dicee-SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  Dicee-SwiftUI
+//
+//  Created by 박새별 on 2023/03/19.
+//
+
+import SwiftUI
+
+// swiftlint:disable: type_name
+struct K {
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+        static let background = UIColor(red: 0.10, green: 0.74, blue: 0.61, alpha: 1.00)
+        static let titleText = Color.white
+    }
+
+    struct Images {
+        static let appIcon = "appIcon"
+        static let background = "background"
+        static let dice1 = "dice1"
+        static let dice2 = "dice2"
+        static let dice3 = "dice3"
+        static let dice4 = "dice4"
+        static let dice5 = "dice5"
+        static let dice6 = "dice6"
+        static let diceeLogo = "diceeLogo"
+    }
+}
--- Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -8,19 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 3
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image(K.Images.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack(alignment: .center) {
+                Image(K.Images.diceeLogo)
+                Spacer()
+                HStack(alignment: .center, spacing: 20.0) {
+                    DiceView(image: getDiceImage(number: leftDiceNumber)!)
+                    DiceView(image: getDiceImage(number: rightDiceNumber)!)
+                }.padding(.horizontal)
+                Spacer()
+//                Button("Roll", action: {
+//                    leftDiceNumber = randomDice()
+//                    rightDiceNumber = randomDice()
+//                }).font(.system(size: 50))
+//                    .fontWeight(.heavy)
+//                    .foregroundColor(.white)
+//                    .padding(.horizontal)
+//                    .background(Color.red)
+
+                Button {
+                    leftDiceNumber = randomDice()
+                    rightDiceNumber = randomDice()
+                } label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                    
+                Text("").font(.system(size: 0))
+            }
         }
-        .padding()
+    }
+}
+
+func randomDice() -> Int {
+    Int.random(in: 1...6)
+}
+
+func getDiceImage(number: Int) -> String? {
+    switch number {
+    case 1: return K.Images.dice1
+    case 2: return K.Images.dice2
+    case 3: return K.Images.dice3
+    case 4: return K.Images.dice4
+    case 5: return K.Images.dice5
+    case 6: return K.Images.dice6
+    default: return nil
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let image: String
+
+    var body: some View {
+        Image(image)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
