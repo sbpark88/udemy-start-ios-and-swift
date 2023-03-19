af4eb5305663b93cd34198c7fbdc5eea15a421f7
sbpark88 <saebyulpark88@gmail.com>
2023-03-19 09:38:48 +0900

    Add: Hacker News-SwiftUI

---
+++ HackerNews/HackerNews/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  HackerNews
+//
+//  Created by 박새별 on 2023/03/19.
+//
+
+import SwiftUI
+
+// swiftlint:disable: type_name
+struct K {
+
+}
---
+++ HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by 박새별 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
