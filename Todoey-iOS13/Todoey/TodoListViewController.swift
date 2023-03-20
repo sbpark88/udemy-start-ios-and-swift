0a030092a6cd5ac3b65208768d81e261adc5b484
sbpark88 <saebyulpark88@gmail.com>
2023-03-21 00:03:48 +0900

    Feat: UITableView Select List

--- Todoey-iOS13/Todoey/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "destroy Demogorgon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = UIColor.cyan
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
     }
-
-
+    
+    // MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
