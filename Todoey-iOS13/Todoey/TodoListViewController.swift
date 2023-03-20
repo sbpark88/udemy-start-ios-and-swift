940a808cf8c64ea7e247a0b6eea4278eae445064
sbpark88 <saebyulpark88@gmail.com>
2023-03-21 06:47:33 +0900

    Refactor: Extract methods using extensions

--- Todoey-iOS13/Todoey/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -17,34 +17,6 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view.
         setTintColor()
         
-        
-    }
-    
-    // MARK - Tableview Datasource Methods
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        itemArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
-        cell.textLabel?.text = itemArray[indexPath.row]
-        
-        return cell
-    }
-    
-    // MARK: Tableview Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
-        
-        tableView.cellForRow(at: indexPath)?.accessoryType
-        = tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.none
-        ? .checkmark
-        : .none
-        
-        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
@@ -59,5 +31,36 @@ extension TodoListViewController {
         appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         navigationItem.standardAppearance = appearance
         navigationItem.scrollEdgeAppearance = appearance
+    }
+}
+
+// MARK - Tableview Datasource Methods
+
+extension TodoListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+}
+
+// MARK: Tableview Delegate Methods
+
+extension TodoListViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType
+        = tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.none
+        ? .checkmark
+        : .none
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
