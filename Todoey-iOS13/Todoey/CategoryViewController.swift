cc2210e376e3e0e1e60c04c0e92056ecfb58a491
sbpark88 <saebyulpark88@gmail.com>
2023-03-28 07:39:48 +0900

    Add: Append New CoreData Entity

---
+++ Todoey-iOS13/Todoey/CategoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by 박새별 on 2023/03/28.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    // MARK: TableView Datasource Methods
+    
+    
+    
+    // MARK: TableView Delegate Methods
+    
+    
+    
+    // MARK: TableView Manipulation Methods
+    
+    
+}
---
+++ Todoey-iOS13/Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by 박새별 on 2023/03/28.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Catetory)
+class Category: NSManagedObject {
+    
+}
