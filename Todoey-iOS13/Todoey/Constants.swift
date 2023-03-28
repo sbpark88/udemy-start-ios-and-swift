cb5f1e665944978078039ff796205d8ceaca2504
sbpark88 <saebyulpark88@gmail.com>
2023-03-28 12:51:03 +0900

    Refactor: Remove the hard coding using Constants.swift

--- Todoey-iOS13/Todoey/Constants.swift
+++ Todoey-iOS13/Todoey/Constants.swift
@@ -12,4 +12,24 @@ struct K {
     struct FileManager {
         static let todoList = "TodoList.plist"
     }
+    
+    struct CategoryView {
+        static let cellName = "CategoryCell"
+        
+        struct alert {
+            static let title = "Add New Todoey Category"
+            static let placeholder = "Create new category"
+            static let button = "Add Category"
+        }
+    }
+    
+    struct TodoListView {
+        static let cellName = "ToDoItemCell"
+        
+        struct alert {
+            static let title = "Add New Todoey Item"
+            static let placeholder = "Create new item"
+            static let button = "Add Item"
+        }
+    }
 }
--- Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class TodoListViewController: UITableViewController, UISearchBarDelegate, UIPickerViewDelegate, UIImagePickerControllerDelegate {
+class TodoListViewController: UITableViewController, UISearchBarDelegate, UIPickerViewDelegate, UIImagePickerControllerDelegate, TintSettings {
     
     var itemArray = [TodoeyItem]()
     
@@ -34,18 +34,18 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate, UIPick
         
         // < Modal Header >
         // likes document.createElement()
-        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: K.TodoListView.alert.title, message: "", preferredStyle: .alert)
         
         // < Modal Body >
         // likes element.setAttribute('attribute', 'value')
         alert.addTextField { textField in // likes <input type="text" placeholder="Create new item">
-            textField.placeholder = "Create new item"
+            textField.placeholder = K.TodoListView.alert.placeholder
             inputText = textField
         }
         
         // Modal Footer Button
         // just function
-        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+        let action = UIAlertAction(title: K.TodoListView.alert.button, style: .default) { action in
             // what will happen once the user clicks the Add Todoey button on our UIAlert
             guard let newTodoey = inputText.text, newTodoey != "" else { return }
             let item = TodoeyItem(context: self.context)
@@ -65,19 +65,6 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate, UIPick
     
 }
 
-// MARK: Set Tint Color
-
-extension TodoListViewController {
-    func setTintColor() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithTransparentBackground()
-        appearance.backgroundColor = UIColor.cyan
-        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navigationItem.standardAppearance = appearance
-        navigationItem.scrollEdgeAppearance = appearance
-    }
-}
-
 // MARK - Tableview Datasource Methods
 
 extension TodoListViewController {
@@ -86,7 +73,7 @@ extension TodoListViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.TodoListView.cellName, for: indexPath)
         let item = itemArray[indexPath.row]
         
         cell.textLabel?.text = item.title
