65173b4cc482666af0795c4214f3b7b0eb197506
sbpark88 <saebyulpark88@gmail.com>
2023-03-28 12:52:35 +0900

    Update: Pop category enrollment modal and print lists

--- Todoey-iOS13/Todoey/CategoryViewController.swift
+++ Todoey-iOS13/Todoey/CategoryViewController.swift
@@ -10,23 +10,95 @@ import UIKit
 import CoreData
 
 class CategoryViewController: UITableViewController, TintSettings {
-
+    
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setTintColor()
+        loadCategory()
     }
-
-    // MARK: TableView Datasource Methods
-    
     
     // MARK: TableView Manipulation Methods
     
     
     // MARK: Add New Categories
+    // TODO: Extract to external structure
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        var inputText = UITextField()
+        
+        let alert = UIAlertController(title: K.CategoryView.alert.title, message: "", preferredStyle: .alert)
+        
+        alert.addTextField { textField in
+            textField.placeholder = K.CategoryView.alert.placeholder
+            inputText = textField
+        }
+        
+        let action = UIAlertAction(title: K.CategoryView.alert.button, style: .default) { action in
+            guard let newCategory = inputText.text, !newCategory.isEmpty else { return }
+            let category = Category(context: self.context)
+            category.name = newCategory
+            self.categories.append(category)
+            
+            self.saveCategory()
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true)
     }
     
-    // MARK: TableView Delegate Methods
+}
 
+// MARK: TableView Datasource Methods
+
+extension CategoryViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.CategoryView.cellName, for: indexPath)
+        let category = categories[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+}
+
+// MARK: TableView Delegate Methods
+
+extension CategoryViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(categories[indexPath.row])
+    }
+}
+
+// MARK: Control Core Data
+
+extension CategoryViewController {
+    
+    func saveCategory() {
+        // TODO: Save the data into Core Data
+        tableView.reloadData()
+    }
+    
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context\(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func deleteCategory(_ category: Category) {
+        // TODO: Delete the data from Core Data
+        tableView.reloadData()
+    }
+    
 }
--- Todoey-iOS13/Todoey/Data Model/Category.swift
+++ Todoey-iOS13/Todoey/Data Model/Category.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-@objc(Catetory)
+@objc(Category)
 class Category: NSManagedObject {
     
 }
